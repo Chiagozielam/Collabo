415250b053bb2dda2e33f6626f5447786845f1d3
Daniel Don <dandons25@gmail.com>
2019-06-04 12:41:46 +0100

    (collabo): added the current files for my collabo project including: .gitignore pp.js confifg/database.js models/Gig.js package-lock.json package.json style.css showcase.jpg showcase1.jpg gigs.js dd.handlebars gigs.handlebars index.handlebars landing.handlebars main.handlebars

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/gigs', require('./routes/gigs'))
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
